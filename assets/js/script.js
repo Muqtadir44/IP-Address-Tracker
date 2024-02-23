2136584101f29f7c344133c42f4a89e580a6c806
Muqtadir <kalhoromuqtadir125@gmail.com>
2024-02-23 17:07:53 +0500

    IP Address Tracker set

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,6 +16,11 @@ $(document).ready(function () {
         event.preventDefault();
         $('#cover-spin').show(0)
 
+        $('#ip-info').text("");
+        $('#location-info').text("");
+        $('#isp-info').text("");
+        $('#timezone-info').text("");
+
         var flag;
         flag = true;
 
@@ -36,24 +41,23 @@ $(document).ready(function () {
 
         if (flag) {
             $('#cover-spin').show(0)
-
             const apiKey = 'at_cN9sbigpoMYpA6dMC8QeqMzeMnJ2e';
 
             // ? -- Sending location Requset --
-
             $.ajax({
                 url: `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&ipAddress=${ip_value}`,
                 type: 'GET',
                 dataType: 'json',
                 error: (error) => {
                     $('#cover-spin').hide(0)
-
-                    displayError(error);
+                    responseError(error);
                 },
                 success: (data) => {
-                    $('#cover-spin').hide(0)
 
-                    renderResults(data);
+                    $('#cover-spin').hide(0)
+                    $('#info').css('display','flex')
+                    $('#info').prop('data-aos-delay')
+                    responseSuccess(data);
                 },
             })
 
@@ -63,13 +67,7 @@ $(document).ready(function () {
     })
     // ? -- Submiting Form --End--
 
-    function renderResults(data) {
-
-        console.log(data)
-        // if (data.error) {
-        //     throw(`${data.reason}`);
-        // }
-
+    function responseSuccess(data) {
         $('#ip-info').text(data.ip);
         $('#location-info').text(`${data.location.city}, ${data.location.country}, ${data.location.region}`);
         $('#isp-info').text(data.isp);
@@ -82,7 +80,7 @@ $(document).ready(function () {
             .openPopup();
     }
 
-    function displayError(error) {
+    function responseError(error) {
         var msg = error.responseJSON.messages;
         iziToast.error({
             theme: 'light',
